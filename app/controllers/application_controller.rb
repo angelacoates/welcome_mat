da8713c943b26847cdd1d777e51ad5854593a742
Angela Coates <angela@angelamarii.com>
2017-04-15 14:03:40 -0400

    added strong params for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :first_name, :last_name, :phone, :address, :address_details, :city, :state, :zip] )
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :first_name, :last_name, :phone, :address, :address_details, :city, :state, :zip] )
+  end
 end

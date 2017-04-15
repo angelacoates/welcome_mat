23d043bbd80b594501bd1239742fc9f61ef240ae
Angela Coates <angela@angelamarii.com>
2017-04-15 10:37:14 -0400

    added validations

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,3 +1,9 @@
 class Home < ApplicationRecord
   include ImageUploader[:image]
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :zipcode, presence: true
 end

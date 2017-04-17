a291b333cb20230e970228064cb4526984abea9a
Angela Coates <angela@angelamarii.com>
2017-04-16 22:38:28 -0400

    trying to change format stuck on making the images to fit all same size

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,1 @@
 
-(1..100).each do |i|
-Home.create!(address: "#{i} Main St",
-		city: "Boston",
-		state: "MA",
-		price: i * 100000,
-		description: "It is a good house",
-		zipcode: '665544',
-		user_id: 1
-	    )
-end

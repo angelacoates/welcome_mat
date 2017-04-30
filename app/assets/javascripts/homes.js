292c098068d3fa73c5cf0c328d96f304855bb72f
Angela Coates <angela@angelamarii.com>
2017-04-29 21:21:53 -0400

    console is responsive, added render

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  $('.media_heading').on('click', function(event){
+    $(this).css({color:'orange'})
+  })
+
+  $("#search").on('input', function(event){
+    let searchValue = $(this).val()
+
+    console.log(`you are searching for ${searchValue}`)
+    
+    $.ajax({
+      url: '/homes',
+      data: { search: searchValue },
+      dataType: 'script'
+    })
+    })
+  })
